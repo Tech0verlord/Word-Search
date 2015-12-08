639d8f2227592257a36a80abd1855eec8e1e1dd1
Tech0verlord <elvin1396@gmail.com>
2015-12-08 13:29:14 -0500

    Added function to read in dictionary
    Made the program able to read in a dictionary by a defined local file,
    and dynamically allocate an array for the dictionary. Also, created a
    test function to determine if the dictionary is being read in or not.
    
    TODO: Change where Clion compiles the executible for the sake of being
    able to read in the dictionary without having to copy files constantly
    between directories

--- wordsearch.c
+++ wordsearch.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void binarySearch();
+char** readDictionary(char fileName[]);
+void testPrint(char** words);
+
+#define DICTIONARY_FILE "dictionary.txt"
+
+const int minSIZE = 4;
+const int maxSIZE = 19;
+
+int dictionarySize;
+
 
 
 
@@ -8,7 +19,43 @@
 int main(){
 
 
+    char** dictionary = readDictionary(DICTIONARY_FILE);
+
+    printf("DICTIONARY SIZE: %d\n", dictionarySize);
+    //testPrint(dictionary);
+
+
 
     return 0;
+
+}
+
+char ** readDictionary(char fileName[]){
+
+    int i;
+    FILE* dict = fopen(fileName, "r");
+
+    fscanf(dict,"%d",&dictionarySize);
+
+    char** words = malloc(sizeof(char*)*dictionarySize);
+
+    for(i=0;i<dictionarySize;i++){
+        words[i] = malloc(sizeof(char)*maxSIZE+1);
+        fscanf(dict,"%s",words[i]);
+    }
+
+    fclose(dict);
+
+    return words;
+
+}
+
+void testPrint(char** words){
+
+    int i;
+
+    for(i=0;i<dictionarySize;i++){
+        printf("Word %d: %s\n", i, words[i]);
+    }
 
 }
